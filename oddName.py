64a0c9053c560af85a3f83f7e2fd9104f2471c47
Sean Abbey <sean.abbey@my.jcu.edu.au>
2016-08-15 10:25:51 +1000

    Add broken score Add Function Edit oddName Add function Add temperatures Add Function

--- oddName.py
+++ oddName.py
@@ -1,7 +1,15 @@
 """Sean"""
+def main():
 
-name = str(input("What is your name? "))
-while len(name) < 1:
+    get_name()
+
+
+def get_name():
     name = str(input("What is your name? "))
-else:
-    print(name[1:len(name):2])
+    while len(name) < 1:
+        name = str(input("What is your name? "))
+    else:
+        print(name[1:len(name):2])
+
+
+main()
---
+++ temperatures.py
@@ -0,0 +1,35 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+def main():
+
+    MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ (for quit)"
+    print(MENU)
+    choice = input(">>> ").upper()
+
+    while choice != "Q":
+        if choice == "C":
+            convert_celsius()
+        elif choice == "F":
+            convert_fahrenheit()
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def convert_celsius():
+    global celsius, fahrenheit
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print("Result: {:.2f} F".format(fahrenheit))
+
+
+def convert_fahrenheit():
+    global fahrenheit, celsius
+    fahrenheit = float(input("Farenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print("Result: {:.2f} C".format(celsius))
+
+main()
