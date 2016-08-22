c2744d99cf97802a6eee588fe90e807aea99f7fd
Sean Abbey <sean.abbey@my.jcu.edu.au>
2016-08-22 10:38:44 +1000

    Edit For Loop

--- quick_pick.py
+++ quick_pick.py
@@ -5,7 +5,7 @@ def main():
     quickpicks = int(input("How many quick picks do you wish to generate? "))
     numbers = []
     import random
-    for i in range(quickpicks):
+    for line in range(quickpicks):
         for i in range(AMOUNT):
             num = random.randint(MIN, MAX)
             while num in numbers:
